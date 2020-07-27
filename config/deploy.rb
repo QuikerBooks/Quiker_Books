de90c91f8bcf4f2f91204e2d747de108b76436f2
Ali Hassan Mirza <ali.hassan.mirzaa@gmail.com>
2020-07-27 21:28:34 +0500

    Repo URL changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :repo_url,        'git@github.com:ali-hassan/quicker_book.git'
+set :repo_url,        'git@github.com:QuikerBooks/Quiker_Books.git'
 set :application,     'quicker_books'
 set :user,            'ubuntu'
 set :puma_threads,    [4, 16]
